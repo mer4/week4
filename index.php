d578c668481fc9cce428ae53da662692bf946810
michael e rogowski wis student <mer4@afsconnect2.njit.edu>
2017-10-04 21:43:56 -0400

    update 2

--- index.php
+++ index.php
@@ -8,6 +8,15 @@ echo "The new value of \$date: ".$newDate."<br>";
 $tar = "2017/05/24";
 echo "The value of \$tar: ".$tar."<br>";
 
+$comp = strcmp($date,$tar);
+if($comp > 0) {
+	echo "the future<br>";
+}elseif($comp < 0) {
+	echo "the past<br>";
+}elseif($comp == 0) {
+	echo "Oops<br>";
+}
+
 $year = array("2012", "396", "300","2000", "1100", "1089");
 echo "The value of \$year: ";
 print_r($year)
