5e636a85a54a8df28c8bf46e5aa709b94225cd80
michael e rogowski wis student <mer4@afsconnect2.njit.edu>
2017-10-05 15:01:08 -0400

    updated 2

--- index.php
+++ index.php
@@ -3,17 +3,28 @@ $date =  date('Y-m-d', time());
 echo "The value of \$date: ".$date."<br>";
 
 $newDate = str_replace("-","/",$date);
-echo "The new value of \$date: ".$newDate."<br>";
+echo "The value of \$newDate: ".$newDate."<br>";
 
 $tar = "2017/05/24";
 echo "The value of \$tar: ".$tar."<br>";
 
 $comp = strcmp($date,$tar);
+echo 'Value: ' .  $comp . '<br>';
 if($comp > 0) {
 	echo "the future<br>";
 }elseif($comp < 0) {
 	echo "the past<br>";
 }elseif($comp == 0) {
+	echo "Oops<br>";
+}
+
+$otherComp = strcmp($newDate,$tar);
+echo 'Value: ' .  $otherComp . '<br>';
+if($otherComp > 0) {
+        echo "the future<br>";
+}elseif($otherComp < 0) {
+	echo "the past<br>";
+}elseif($otherComp == 0) {
 	echo "Oops<br>";
 }
 
